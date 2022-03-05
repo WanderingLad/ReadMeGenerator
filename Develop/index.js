cc118d05c3e2a4ff7e226ff0b2c909ea954deca9
James Reilly <jreilly2112@gmail.com>
2022-03-04 22:08:25 -0500

    updated how the file was generated

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,11 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 const api = require('./utils/api.js');
 
-// TODO: Create an array of questions for user input
 const questions = [
-    /* {
+    {
         type: 'input',
         message: "What is your project's title?",
         name: 'title',
@@ -74,7 +72,7 @@ const questions = [
     {
         type: 'input',
         message: "Describe how other developers can contribute to this project.",
-        name: 'contributes',
+        name: 'contributions',
         validate: function (answer)
         {
             if(answer)
@@ -108,8 +106,19 @@ const questions = [
         message: "Choose a license type for your project.",
         choices: ['I need to work in a community.', 'I want it simple and permissive.', 'I care about sharing improvements.', "My project isn't a software.", 'I do not want to choose'],
         name: 'licenses',
+        validate: function (answer)
+        {
+            if(answer)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     },  
-    */{
+    {
         type: 'input',
         message: "What is your GitHub username?",
         name: 'githubName',
@@ -124,7 +133,7 @@ const questions = [
                 return false;
             }
         }
-    }/*,
+    },
     {
         type: 'input',
         message: "What is your repo's name?",
@@ -140,10 +149,9 @@ const questions = [
                 return false;
             }
         }
-    } */
+    }
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(file, data) {
     fs.writeFile(file, data, err => {
         if (err) {
@@ -156,20 +164,15 @@ function writeToFile(file, data) {
 
 const writeFileAsync = util.promisify(writeToFile);
 
-// TODO: Create a function to initialize app
 async function init() {
     try {
 
-        // Prompt Inquirer questions
         const readMe = await inquirer.prompt(questions);
     
-        // Call GitHub api for user info
         const user = await api.getUser(readMe);
     
-        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
         const markdown = generateMarkdown(readMe, user);
     
-        // Write markdown to file
         await writeFileAsync(`${readMe.githubName}ReadMe.md`, markdown);
 
     } catch (error) {
@@ -177,5 +180,4 @@ async function init() {
     }
 };
 
-// Function call to initialize app
 init();
